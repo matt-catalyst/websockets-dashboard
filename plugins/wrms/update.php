823baffb3cbeab3fa57a09a1395f6e79e5678fe9
Aaron Barnes <aaronb@catalyst.net.nz>
2012-10-01 21:23:30 +1300

    plugins/wrms: Display useful and up-to-date information

--- plugins/wrms/update.php
+++ plugins/wrms/update.php
@@ -3,49 +3,88 @@
 require_once(dirname(dirname(__FILE__)).'/lib.php');
 require_once(dirname(__FILE__).'/config.php');
 
-$lastwr = null;
+// Cache last 20 WRs to check for changed WRs
+$lastwrs = array();
+
+// Cols we care about
+$cols = array(
+    'request_id',
+    'last_activity_epoch',
+    'organisation_code',
+    'system_name',
+    'system_code',
+    'status_desc',
+    'brief',
+    'ranking'
+);
+
+// Base url
+$url = $config->url.
+        '/api2/report'.
+        '?report_type=request'.
+        '&page_size=100'.
+        '&display_fields='.implode(',', $cols).
+        '&interested_users='.$config->interested_user;
+
+$reports = array(
+    '&last_status=N&order_by=last_activity_epoch&order_direction=desc',
+    '&last_status=N&order_by=last_activity_epoch&order_direction=desc&page_no=2',
+    '&last_status=N&order_by=last_activity_epoch&order_direction=desc&page_no=3',
+    '&last_status=L&order_by=ranking&order_direction=desc'
+);
 
 // Wait 1-10 seconds then send random WR data
 while(1) {
 
     print "API call\n";
 
-    $url = '/api2/report?report_type=request&page_size=20&display_fields=request_id,system_code,status_desc,brief&interested_users='.$config->interested_user;
+    // Number "seen"
+    $seen = 0;
+    $send = false;
 
-    $ch = curl_init($config->url.$url);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
-    curl_setopt($ch, CURLOPT_COOKIE, 'wrms3_auth='.$config->secret);
-    $result = curl_exec($ch);
-    curl_close($ch);
+    foreach ($reports as $report) {
+        $ch = curl_init($url.$report);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, True);
+        curl_setopt($ch, CURLOPT_COOKIE, 'wrms3_auth='.$config->secret);
+        $result = curl_exec($ch);
+        curl_close($ch);
 
-    $response = json_decode($result);
-    $data = $response->response->results;
+        $response = json_decode($result);
+        $data = $response->response->results;
 
-    // Reverse data
-    $data = array_reverse($data);
+        foreach ($data as $i => $row) {
 
-    // Data to send
-    $send = array();
-
-    foreach ($data as $row) {
-        if ($row->request_id <= $lastwr) {
-            continue;
-        }
-
-        if (!empty($config->ignore_systems)) {
-            if (in_array($row->system_code, $config->ignore_systems)) {
-                continue;
+            // If that's 20 new/edited/unchanged, break
+            if ($seen >= 20) {
+                break 2;
             }
-        }
 
-        $row->request_url = "{$config->url}/wr.php?request_id={$row->request_id}";
-        $send[] = $row;
+            // Check if ignored system
+            if (!empty($config->ignore_orgs)) {
+                if (in_array($row->organisation_code, $config->ignore_orgs)) {
+                    continue;
+                }
+            }
+
+            // Add URL
+            $row->request_url = "{$config->url}/wr.php?request_id={$row->request_id}";
+
+            // Check if it doesn't match what is in $lastwrs
+            if (!isset($lastwrs[$seen]) || $row != $lastwrs[$seen]) {
+                // Update $lastwrs and trigger sending of new version
+                $send = true;
+                $lastwrs[$seen] = $row;
+            }
+
+            ++$seen;
+        }
     }
 
-    if (!empty($send)) {
-        print count($send)." updates sent\n";
-        dashboard_push_data('wrms', $send, $multiple=true);
-        $lastwr = $row->request_id;
+    if ($send) {
+        print "Update sent\n";
+        // Reverse order of WRs (as they will be prepended one at a time) and
+        // pad the array to flush out the cache
+        dashboard_push_data('wrms', array_pad(array_reverse($lastwrs), -20, 1), $multiple=true);
     }
 
     sleep(60);
